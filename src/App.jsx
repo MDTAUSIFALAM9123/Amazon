44f4acce21d6b96d3d39aab6f224eb13305aad80
MDTAUSIFALAM9123 <mdtausifalam9123@gmail.com>
2025-06-12 08:04:19 +0530

    Updated data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Homepages';
+export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
 }
-export default App;
